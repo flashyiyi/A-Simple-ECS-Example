b0d97192cf9ec4de6fa720609779344f38f2d1a6
flashyiyi <yiyiflashstudio@163.com>
2018-01-18 19:39:49 +0800

    调整函数顺序

--- GameWorld.cs
+++ GameWorld.cs
@@ -107,97 +107,6 @@ public class SystemBase
         this.world = world;
     }
 }
-//增删物体和场景初始化
-public class EntitySystem : SystemBase
-{
-    public EntitySystem(GameWorld world) : base(world) { }
-
-    public void AddEntity(Entity e)
-    {
-        world.entitys.DelayAdd(e);
-        world.gameObjectSystem.Add(e.gameObject, e.position, e.size, e.color);
-    }
-
-    public void RemoveEntity(Entity e)
-    {
-        world.entitys.DelayRemove(e);
-        if (e.gameObject != null)
-            world.gameObjectSystem.Remove(e.gameObject);
-    }
-
-    public void AddRandomEntity()
-    {
-        Entity e = new Entity();
-        e.size.value = 0.025f;
-        e.team.id = 0;
-        e.position.value = new Vector2(Random.Range(world.screenRect.xMin + e.size.value, world.screenRect.xMax - e.size.value), Random.Range(world.screenRect.yMin + e.size.value, world.screenRect.yMax - e.size.value));
-        AddEntity(e);
-    }
-
-    public void AddMoveAbleEnity(MoveAbleEntity e)
-    {
-        this.AddEntity(e);
-        world.playerEntitys.Add(e);
-
-        world.moveSystem.Add(e.speed);
-        world.gameObjectSystem.SetToTop(e.gameObject);
-    }
-
-    public void InitScene()
-    {
-        for (int i = 0; i < 50; i++)
-        {
-            AddRandomEntity();
-        }
-
-        for (int i = 0; i < 2; i++)
-        {
-            MoveAbleEntity playerEntity = new MoveAbleEntity();
-            playerEntity.position.value = Vector2.zero;
-            playerEntity.size.value = 0.05f;
-            playerEntity.color.value = Color.yellow;
-            playerEntity.speed.maxValue = 1f;
-            playerEntity.team.id = 1;
-            playerEntity.position.value = new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
-            AddMoveAbleEnity(playerEntity);
-        }
-    }
-}
-
-//和Unity显示部分的桥接
-public class GameObjectSystem : SystemBase
-{
-    public GameObjectSystem(GameWorld world) : base(world) { }
-    public void Add(GameObjectComponent e, PositionComponent position, SizeComponent size, ColorComponent color)
-    {
-        e.gameObject = new GameObject("Entity");
-        e.transform = e.gameObject.transform;
-        e.transform.localScale = Vector2.one * 0.001f;
-        e.spriteRenderer = e.gameObject.AddComponent<SpriteRenderer>();
-        e.spriteRenderer.sprite = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Knob.psd");
-        Update(e, position, size, color);
-    }
-
-    public void Remove(GameObjectComponent go)
-    {
-        GameObject.Destroy(go.gameObject);
-        go.transform = null;
-        go.gameObject = null;
-        go.spriteRenderer = null;
-    }
-
-    public void Update(GameObjectComponent go, PositionComponent position, SizeComponent size, ColorComponent color)
-    {
-        go.transform.position = position.value;
-        go.transform.localScale = Vector2.one * Mathf.MoveTowards(go.transform.localScale.x, size.value * 11f, Mathf.Max(0.01f, Mathf.Abs(go.transform.localScale.x - size.value)) * 10f * Time.deltaTime);
-        go.spriteRenderer.color = color.value;
-    }
-
-    public void SetToTop(GameObjectComponent go)
-    {
-        go.gameObject.AddComponent<SortingGroup>().sortingOrder = 1;
-    }
-}
 
 //移动
 public class MoveSystem : SystemBase
@@ -205,7 +114,12 @@ public class MoveSystem : SystemBase
     public MoveSystem(GameWorld world) : base(world) { }
     public void Add(SpeedComponent speed)
     {
-        world.speeds.Add(speed);
+        world.speeds.DelayAdd(speed);
+    }
+
+    public void Remove(SpeedComponent speed)
+    {
+        world.speeds.DelayRemove(speed);
     }
 
     public void Update(SpeedComponent speed, PositionComponent position, SizeComponent size)
@@ -313,6 +227,98 @@ public class EatingSystem : SystemBase
         comp.endOffest = Vector2.Lerp(source.value, target.value, 0.5f) - target.value;
         comp.Start();
         world.eatings.DelayAdd(comp);
+    }
+}
+
+//和Unity显示部分的桥接
+public class GameObjectSystem : SystemBase
+{
+    public GameObjectSystem(GameWorld world) : base(world) { }
+    public void Add(GameObjectComponent e, PositionComponent position, SizeComponent size, ColorComponent color)
+    {
+        e.gameObject = new GameObject("Entity");
+        e.transform = e.gameObject.transform;
+        e.transform.localScale = Vector2.one * 0.001f;
+        e.spriteRenderer = e.gameObject.AddComponent<SpriteRenderer>();
+        e.spriteRenderer.sprite = UnityEditor.AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/Knob.psd");
+        Update(e, position, size, color);
+    }
+
+    public void Remove(GameObjectComponent go)
+    {
+        GameObject.Destroy(go.gameObject);
+        go.transform = null;
+        go.gameObject = null;
+        go.spriteRenderer = null;
+    }
+
+    public void Update(GameObjectComponent go, PositionComponent position, SizeComponent size, ColorComponent color)
+    {
+        go.transform.position = position.value;
+        go.transform.localScale = Vector2.one * Mathf.MoveTowards(go.transform.localScale.x, size.value * 11f, Mathf.Max(0.01f, Mathf.Abs(go.transform.localScale.x - size.value)) * 10f * Time.deltaTime);
+        go.spriteRenderer.color = color.value;
+    }
+
+    public void SetToTop(GameObjectComponent go)
+    {
+        go.gameObject.AddComponent<SortingGroup>().sortingOrder = 1;
+    }
+}
+
+//增删物体和场景初始化
+public class EntitySystem : SystemBase
+{
+    public EntitySystem(GameWorld world) : base(world) { }
+
+    public void AddEntity(Entity e)
+    {
+        world.entitys.DelayAdd(e);
+        world.gameObjectSystem.Add(e.gameObject, e.position, e.size, e.color);
+    }
+
+    public void RemoveEntity(Entity e)
+    {
+        world.entitys.DelayRemove(e);
+        if (e.gameObject != null)
+            world.gameObjectSystem.Remove(e.gameObject);
+    }
+
+    public void AddRandomEntity()
+    {
+        Entity e = new Entity();
+        e.size.value = 0.025f;
+        e.team.id = 0;
+        e.position.value = new Vector2(Random.Range(world.screenRect.xMin + e.size.value, world.screenRect.xMax - e.size.value), Random.Range(world.screenRect.yMin + e.size.value, world.screenRect.yMax - e.size.value));
+        AddEntity(e);
+    }
+
+    public void AddMoveAbleEnity(MoveAbleEntity e)
+    {
+        this.AddEntity(e);
+        world.playerEntitys.Add(e);
+
+        world.moveSystem.Add(e.speed);
+        world.gameObjectSystem.SetToTop(e.gameObject);
+    }
+
+    public void InitScene()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            AddRandomEntity();
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            MoveAbleEntity playerEntity = new MoveAbleEntity();
+            playerEntity.position.value = Vector2.zero;
+            playerEntity.size.value = 0.05f;
+            playerEntity.color.value = Color.yellow;
+            playerEntity.speed.maxValue = 1f;
+            playerEntity.team.id = 1;
+            playerEntity.position.value = new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+            AddMoveAbleEnity(playerEntity);
+        }
     }
 }
 #endregion
