68b438ceeefdecb9110c02210679f840ebbf5b69
flashyiyi <yiyiflashstudio@163.com>
2018-01-18 15:13:14 +0800

    First Comiit

---
+++ DList.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class DItem : IDItem
+{
+    public bool destroyed { get; set; }
+}
+
+public interface IDItem
+{
+    bool destroyed { get; set; }
+}
+
+/// <summary>
+/// 提供延迟删除List元素的功能
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class DList<T> : List<T> where T:IDItem
+{
+    bool m_RemoveDirty = false;
+    bool m_AddDirty = false;
+    List<T> delayAddList = new List<T>();
+
+    public void DelayAdd(T item)
+    {
+        delayAddList.Add(item);
+        m_AddDirty = true;
+    }
+
+    public void DelayRemove(T item)
+    {
+        item.destroyed = true;
+        m_RemoveDirty = true;
+    }
+
+    public void DelayRemoveAt(int index)
+    {
+        Remove(this[index]);
+    }
+
+    public void ApplyDelayCommands()
+    {
+        if (this.m_RemoveDirty)
+        {
+            int count = this.Count;
+            int i = 0, j = 0;
+            while (i < count)
+            {
+                if (this[i].destroyed)
+                {
+                    j = i + 1;
+                    while (j < count)
+                    {
+                        if (!this[j].destroyed)
+                        {
+                            this[i] = this[j];
+                            i++;
+                        }
+                        j++;
+                    }
+                    this.RemoveRange(i, this.Count - i);
+                    break;
+                }
+                i++;
+            }
+            this.m_RemoveDirty = false;
+        }
+
+        if (this.m_AddDirty)
+        {
+            this.AddRange(delayAddList);
+            this.delayAddList.Clear();
+            this.m_AddDirty = false;
+        }
+    }
+}
---
+++ GameWorld.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+public class GameWorld : MonoBehaviour
+{
+    public DList<Entity> entitys;
+    public DList<SpeedComponent> speeds;
+    public DList<MoveAbleEntity> playerEntitys;
+
+    public DList<EatingComponent> eatings;
+
+    public EntitySystem entitySystem;
+    public MoveSystem moveSystem;
+    public GameObjectSystem gameObjectSystem;
+    public InputSystem inputSystem;
+    public CirclePushSystem circlePushSystem;
+    public EatSystem eatSystem;
+    public EatingSystem eatingSystem;
+
+    public Camera mainCamera;
+    public Rect screenRect;
+
+    void Start ()
+    {
+        if (Camera.main == null)
+        {
+            GameObject go = new GameObject("Camera");
+            mainCamera = go.AddComponent<Camera>();
+        }
+        else
+        {
+            mainCamera = Camera.main;
+        }
+        mainCamera.clearFlags = CameraClearFlags.Color;
+        mainCamera.backgroundColor = Color.black;
+        mainCamera.orthographic = true;
+        mainCamera.orthographicSize = 1f;
+        mainCamera.nearClipPlane = 0f;
+
+        screenRect = Rect.MinMaxRect(-mainCamera.aspect, -1f, mainCamera.aspect, 1f);
+
+        entitys = new DList<Entity>();
+        playerEntitys = new DList<MoveAbleEntity>();
+        speeds = new DList<SpeedComponent>();
+        eatings = new DList<EatingComponent>();
+
+        entitySystem = new EntitySystem(this);
+        moveSystem = new MoveSystem(this);
+        gameObjectSystem = new GameObjectSystem(this);
+
+        inputSystem = new InputSystem(this);
+
+        eatSystem = new EatSystem(this);
+        eatingSystem = new EatingSystem(this);
+        circlePushSystem = new CirclePushSystem(this);
+
+        entitySystem.InitScene();
+        ApplyDelayCommands();
+    }
+    
+    public void ApplyDelayCommands()
+    {
+        entitys.ApplyDelayCommands();
+        playerEntitys.ApplyDelayCommands();
+        speeds.ApplyDelayCommands();
+        eatings.ApplyDelayCommands();
+    }
+
+    void Update ()
+    {
+        //遍历所有Entity并执行所有相关System
+        foreach (Entity item in entitys)
+        {
+            if (item.destroyed)
+                continue;
+
+            gameObjectSystem.Update(item.gameObject, item.position, item.size,item.color);
+        }
+        //多对多关系
+        foreach (MoveAbleEntity player in playerEntitys)
+        {
+            if (player.destroyed)
+                continue;
+
+            inputSystem.Update(player);
+            foreach (Entity item in entitys)
+            {
+                if (item == player || item.destroyed)
+                    continue;
+
+                if (item.team.id == 0) //是食物，执行吃逻辑
+                    eatSystem.Update(player, item);
+                else if (item.team.id == 1) //是玩家控制角色，执行圆推挤逻辑
+                    circlePushSystem.Update(player.position, player.size, item.position, item.size);
+            }
+        }
+        //单独遍历某些Component
+        foreach (SpeedComponent speed in speeds)
+        {
+            if (speed.destroyed)
+                continue;
+
+            MoveAbleEntity moveEnity = speed.entity as MoveAbleEntity;
+            moveSystem.Update(speed, moveEnity.position, moveEnity.size);
+        }
+        //和Entity无关的Component
+        foreach (EatingComponent item in eatings)
+        {
+            if (item.destroyed)
+                continue;
+
+            eatingSystem.Update(item);
+        }
+
+        ApplyDelayCommands();
+    }
+}
